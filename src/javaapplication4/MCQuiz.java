715c393ac02647be3106c7e0f138cbd57ad392ba
ArZal6581 <ArZal6581@8R71Q52>
2026-05-19 11:36:58 -0400

    Initializing all Empty Files

--- src/javaapplication4/MCQuiz.java
+++ src/javaapplication4/MCQuiz.java
@@ -10,63 +10,69 @@ public class MCQuiz extends javax.swing.JFrame {
     public MCQuiz(StudyGUI m) {
         initComponents();
         mainWindow = m;
+        question1.setSelected(true);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        questionNumber = new javax.swing.JLabel();
+        mulitpleChoiceGroup = new javax.swing.ButtonGroup();
+        exitButton = new javax.swing.JButton();
+        questionLabel = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         questionTextArea = new javax.swing.JTextArea();
-        button1 = new javax.swing.JRadioButton();
-        button2 = new javax.swing.JRadioButton();
-        button3 = new javax.swing.JRadioButton();
-        button4 = new javax.swing.JRadioButton();
-        jLabel3 = new javax.swing.JLabel();
+        question1 = new javax.swing.JRadioButton();
+        question2 = new javax.swing.JRadioButton();
+        question3 = new javax.swing.JRadioButton();
+        question4 = new javax.swing.JRadioButton();
+        feedbackLabel = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        feedback = new javax.swing.JTextArea();
-        commitBtn = new javax.swing.JButton();
-        nextBtn = new javax.swing.JButton();
+        feedbackTextField = new javax.swing.JTextArea();
+        checkButton = new javax.swing.JButton();
+        nextButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Exit");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        exitButton.setText("Exit");
+        exitButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                exitButtonActionPerformed(evt);
             }
         });
 
-        questionNumber.setText("Question #");
+        questionLabel.setText("Question #");
 
         questionTextArea.setColumns(20);
         questionTextArea.setRows(5);
         jScrollPane1.setViewportView(questionTextArea);
 
-        button1.setText("jRadioButton1");
+        mulitpleChoiceGroup.add(question1);
+        question1.setText("jRadioButton1");
 
-        button2.setText("jRadioButton2");
+        mulitpleChoiceGroup.add(question2);
+        question2.setText("jRadioButton2");
 
-        button3.setText("jRadioButton3");
+        mulitpleChoiceGroup.add(question3);
+        question3.setText("jRadioButton3");
 
-        button4.setText("jRadioButton4");
+        mulitpleChoiceGroup.add(question4);
+        question4.setText("jRadioButton4");
 
-        jLabel3.setText("Feedback");
+        feedbackLabel.setText("Feedback");
 
-        feedback.setColumns(20);
-        feedback.setRows(5);
-        jScrollPane2.setViewportView(feedback);
+        feedbackTextField.setColumns(20);
+        feedbackTextField.setRows(5);
+        jScrollPane2.setViewportView(feedbackTextField);
 
-        commitBtn.setText("Check");
-        commitBtn.addActionListener(new java.awt.event.ActionListener() {
+        checkButton.setText("Check");
+        checkButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                commitBtnActionPerformed(evt);
+                checkButtonActionPerformed(evt);
             }
         });
 
-        nextBtn.setText("Next");
+        nextButton.setText("Next");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -75,7 +81,7 @@ public class MCQuiz extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(questionNumber)
+                    .addComponent(questionLabel)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
@@ -84,50 +90,50 @@ public class MCQuiz extends javax.swing.JFrame {
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel3))
+                                    .addComponent(feedbackLabel))
                                 .addGap(18, 18, 18)))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(button1)
-                            .addComponent(button2)
-                            .addComponent(button3)
-                            .addComponent(button4)
-                            .addComponent(jButton1)))
+                            .addComponent(question1)
+                            .addComponent(question2)
+                            .addComponent(question3)
+                            .addComponent(question4)
+                            .addComponent(exitButton)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(252, 252, 252)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(nextBtn)
-                            .addComponent(commitBtn))))
+                            .addComponent(nextButton)
+                            .addComponent(checkButton))))
                 .addContainerGap(44, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(questionNumber)
+                .addComponent(questionLabel)
                 .addGap(7, 7, 7)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(button1)
+                        .addComponent(question1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(button2)
+                        .addComponent(question2)
                         .addGap(11, 11, 11)
-                        .addComponent(button3)
+                        .addComponent(question3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(button4))
+                        .addComponent(question4))
                     .addComponent(jScrollPane1))
                 .addGap(16, 16, 16)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(18, 18, Short.MAX_VALUE)
-                        .addComponent(commitBtn)
+                        .addComponent(checkButton)
                         .addGap(11, 11, 11)
-                        .addComponent(nextBtn)
+                        .addComponent(nextButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton1)
+                        .addComponent(exitButton)
                         .addGap(22, 22, 22))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLabel3)
+                        .addComponent(feedbackLabel)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(28, 28, 28))))
@@ -136,29 +142,30 @@ public class MCQuiz extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitButtonActionPerformed
         mainWindow.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_exitButtonActionPerformed
 
-    private void commitBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_commitBtnActionPerformed
+    private void checkButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkButtonActionPerformed
         
-    }//GEN-LAST:event_commitBtnActionPerformed
+    }//GEN-LAST:event_checkButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JRadioButton button1;
-    private javax.swing.JRadioButton button2;
-    private javax.swing.JRadioButton button3;
-    private javax.swing.JRadioButton button4;
-    private javax.swing.JButton commitBtn;
-    private javax.swing.JTextArea feedback;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JButton checkButton;
+    private javax.swing.JButton exitButton;
+    private javax.swing.JLabel feedbackLabel;
+    private javax.swing.JTextArea feedbackTextField;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JButton nextBtn;
-    private javax.swing.JLabel questionNumber;
+    private javax.swing.ButtonGroup mulitpleChoiceGroup;
+    private javax.swing.JButton nextButton;
+    private javax.swing.JRadioButton question1;
+    private javax.swing.JRadioButton question2;
+    private javax.swing.JRadioButton question3;
+    private javax.swing.JRadioButton question4;
+    private javax.swing.JLabel questionLabel;
     private javax.swing.JTextArea questionTextArea;
     // End of variables declaration//GEN-END:variables
 }
